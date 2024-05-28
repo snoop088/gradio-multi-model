8b4663f8564879b7c2392a64d707449fda747976
Nick Dimitrov <snoop088@gmail.com>
2024-05-28 10:33:22 +0300

    switch to gpt-4o

--- app/inference.py
+++ app/inference.py
@@ -21,8 +21,8 @@ def inference(message, history, llm_type, temperature, template=None):
         gpt3_llm = ChatOpenAI(temperature=temperature, model="gpt-3.5-turbo") 
         chain = prompt | gpt3_llm | StrOutputParser()
         response = chain.invoke({"input": sanitised_message})
-    elif (llm_type == 'gpt_4'):
-        gpt4_llm = ChatOpenAI(temperature=temperature, model="gpt-4-0125-preview")
+    elif (llm_type == 'gpt-4o'):
+        gpt4_llm = ChatOpenAI(temperature=temperature, model="gpt-4o")
         chain = prompt | gpt4_llm | StrOutputParser()
         response = chain.invoke({"input": sanitised_message})
     else:
